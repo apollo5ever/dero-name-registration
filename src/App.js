b7eb18f1b065a63a7d8421bb92f4309211dbfd9b
apollo5ever <backend@ambrosia.money>
2022-05-26 00:45:03 -0500

    added logic to account for my sole burned alias, as well as aliases which are too short, and added some confusing info

--- src/App.js
+++ src/App.js
@@ -113,6 +113,16 @@ const App = () => {
      setAvailability("Shucks, this name is unavailable");
    }else{
      setAvailability("This name is available!")
+     console.log("lenght" + name.length)
+     if(name.length <6 ){
+      setAvailability("This name is too short")
+      
+
+   }
+  
+   }
+   if(name === "e=mc²"){
+     setAvailability("This name was foolishly burned by apollo")
    }
    
 
@@ -129,9 +139,12 @@ const App = () => {
     <div>{bridgeInitText}</div>
     <h1>Name Registration</h1>
             <p>Check for availability and register your DERO name</p>
-            <p>Remember names must be 6 characters or longer. This website will tell you "moon" is available but that's a lie. It's not enough characters.</p>
+            <p>Remember names must be 6 characters or longer.</p>
             <p>Names are CASE-sensitive. "Apollo" is not the same as "apollo". Numbers, spaces, special characters, and emojis are allowed.</p>
             <p><i>New!</i> Now you can also transfer a name you already own to another wallet address.</p>
+            <p> Note: You may not send a name to another name. I tried this with e=mc² and now that name is lost forever. It has been burned and nobody can own it now.</p>
+            <p> Now you may be wondering, isn't e=mc² less than six characters? Answer: Dero will count certain special characters as more than 1. For instance someone successfully registered 🅱️🅾️🅾️🅱️🅰️.. </p>
+            <p>This website will not account for that when it tells you that your name is too short. If your special short name is unavailable it will say so. If it says it is too short but you know it will work, then go for it. </p>
             
             <a href="https://chrome.google.com/webstore/detail/dero-rpc-bridge/nmofcfcaegdplgbjnadipebgfbodplpd">Oh and you'll need to download this extension</a>
             
@@ -166,7 +179,7 @@ const App = () => {
       <h3><i>New!</i> Transfer Name</h3>
   <form onSubmit={transfer}>
     <input placeholder="name" id="name" type="text" />
-    <input placeholder="address" id="address" type="text" />
+    <input placeholder="recipient address" id="address" type="text" />
     <button type={"submit"}>Transfer</button>
   </form>
   
